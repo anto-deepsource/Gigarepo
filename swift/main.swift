bdf06f1dfe309a0e37eb79e02d4883c87bf5e213
Anto Christopher <93177734+anto-deepsource@users.noreply.github.com>
2023-10-09 14:31:27 +0530

    Create main.swift
    Signed-off-by: Anto Christopher <93177734+anto-deepsource@users.noreply.github.com>

---
+++ swift/main.swift
@@ -0,0 +1,12 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-books
+
+func addNums(a: Int, b: Int) -> Int {
+  return a + b
+}
+
+func foo() {
+  let max = arr.sorted().last  // trivia
+  let min = /* trivia */ arr.sorted().first
+    let minmax = arr.sorted().last
+}
