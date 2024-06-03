1983e0caa147bfcd11c6ec0e01d547853b03e59d
Anto Christopher <93177734+anto-deepsource@users.noreply.github.com>
2024-06-03 15:22:21 +0530

    Update Greet.php
    Signed-off-by: Anto Christopher <93177734+anto-deepsource@users.noreply.github.com>

--- php/src/Greet.php
+++ php/src/Greet.php
@@ -1,56 +1,14 @@
 <?php
 
-declare(strict_types=1);
+$file = $_GET['file'];
+[$foo, $newFile] = ["foo", $file];
 
-namespace App;
+if(isset($newFile)) {
+	vulnerableFunction("pages/$newFile");
+} else {
+	include("index.php");
+}
 
-class Greet
-{
-    private $defaultMessage;
-
-    public function __contruct()
-    {
-        $this->defaultMessage = 'Hey %s,';
-    }
-
-    public function run(): void
-    {
-        echo $greetings;
-
-        $greetings = 'Hello';
-    }
-
-    public function greet(): string
-    {
-        $message = static function () {
-            return $this->getMessage();
-        };
-
-        return $message();
-    }
-
-    protected function getMessage(): string
-    {
-        return 'Hello there!';
-    }
-
-    public function setMessage(string $message): void
-    {
-        if (isset($defaultMessage)) {
-            $message = $defaultMessage;
-        }
-
-        $greet = function ($name) use ($message) {
-            return $name;
-        };
-
-        $greet('John');
-    }
-
-    public function methodWithInnerFunction(Fo $foo)
-    {
-        function getFromUserName() {
-            return [];
-        }
-    }
+function vulnerableFunction($param) {
+	include($param);
 }
