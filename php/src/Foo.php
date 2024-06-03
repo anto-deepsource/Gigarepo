b651c583fe928b47ae55c7c69b2b02e3dc89233a
Anto Christopher <93177734+anto-deepsource@users.noreply.github.com>
2024-06-03 16:16:17 +0530

    Update Foo.php
    Signed-off-by: Anto Christopher <93177734+anto-deepsource@users.noreply.github.com>

--- php/src/Foo.php
+++ php/src/Foo.php
@@ -4,13 +4,10 @@ $conn = mysqli_connect("localhost", "my_user", "my_password", "world");
 $data = $_REQUEST['data'];
 $vulnerableSql = "SELECT * FROM TEST WHERE test = '{$data}'";
 
+// prepare query
 function query($param) {
-	$query = "SELECT * FROM `plans` WHERE `id` = '{$param}'";
-  $db = mydb::getMe();
-  $rs = $db->queryAssoc($query);
-  $rs = $rs[0];
-
-  return $rs;
+  $query = "SELECT * FROM `plans` WHERE `id` = '{$param}'";
+  return $query;
 }
 
 query($vulnerableSql);
